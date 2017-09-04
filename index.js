d04b68147fb1e3ff231d86c210ce347a551489ac
Faisal <falquaddoomi@ucla.edu>
2017-09-04 17:56:45 +0200

    formatting changes

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite{
-    constructor( opts ){
-        super( opts.game, opts.x || 0, opts.y || 0, opts.key || '', opts.frame || '' )
+    constructor(opts) {
+        super(opts.game, opts.x || 0, opts.y || 0, opts.key || '', opts.frame || '')
 
         // Save options
         this.opts = opts
 
         // Enable player physics
-        this.game.physics.enable( this, Phaser.Physics.ARCADE )
+        this.game.physics.enable(this, Phaser.Physics.ARCADE)
 
         // Player specifics
         this.body.collideWorldBounds = opts.hasOwnProperty('collideWorldBounds') ? opts.collideWorldBounds : true
-        this.body.setSize( opts.width || 64, opts.height || 64 )
+        this.body.setSize(opts.width || 64, opts.height || 64)
         this.health = opts.health || 1
 
         // Shortcut to current state object
         this.state = this.game.state.states[this.game.state.current]
 
         // Set up camera
-        if( opts.hasOwnProperty('follow') && opts.follow === true ){
-            this.game.camera.follow( this )
+        if (opts.hasOwnProperty('follow') && opts.follow === true) {
+            this.game.camera.follow(this)
 
-            if( opts.hasOwnProperty('deadzone') ){
+            if (opts.hasOwnProperty('deadzone')) {
                 const left = opts.deadzone.left <= 1 ? this.game.width * opts.deadzone.left : opts.deadzone.left
                 const width = opts.deadzone.width <= 1 ? this.game.width * opts.deadzone.width : opts.deadzone.width
-                this.game.camera.deadzone = new Phaser.Rectangle( left, 0, width, this.game.height )
+                this.game.camera.deadzone = new Phaser.Rectangle(left, 0, width, this.game.height)
             }
         }
 
@@ -37,23 +37,25 @@ export default class extends Phaser.Sprite{
         this.onJump = opts.hasOwnProperty('onJump') ? opts.onJump : null
 
         // Set up controls
-        if( opts.hasOwnProperty('controls') && opts.controls ){
+        if (opts.hasOwnProperty('controls') && opts.controls) {
 
             this.cursors = this.game.input.keyboard.createCursorKeys()
-            this.jumpButton = this.game.input.keyboard.addKey( Phaser.Keyboard.SPACEBAR )
+            this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
             // Jump
-            this.jumpButton.onDown.add( function(){ this.jump() }, this )
+            this.jumpButton.onDown.add(function() { this.jump() }, this)
 
         }
 
         // Set up where the sprite is facing
         this.facing = 1
 
+        // also make our sprite oriented around the center
+        this.anchor.setTo(0.5)
     }
 
-    jump(){
-        if ( this.currentJump < this.totalJumps ){
+    jump() {
+        if (this.currentJump < this.totalJumps) {
             this.body.velocity.y = this.jumpForce
 
             if (this.onJump) {
@@ -64,34 +66,38 @@ export default class extends Phaser.Sprite{
         }
     }
 
-    die(){
+    die() {
         this.kill()
     }
 
-    update(){
+    update() {
         // basic living/dying checks
-        if( !this.alive ) return
-        if( this.health <= 0 ){
+        if (!this.alive) return
+
+        if (this.health <= 0) {
             this.die()
         }
 
         // reset jump count
-        if( this.body.onFloor() ){
+        if (this.body.onFloor()) {
             this.currentJump = 0
         }
 
-        if( this.opts.hasOwnProperty('controls') && this.opts.controls ){
+        if (this.opts.hasOwnProperty('controls') && this.opts.controls) {
             // horizontal movement
-            if( this.cursors.left.isDown ){
+            if (this.cursors.left.isDown) {
                 this.body.velocity.x = -this.xSpeed
-                this.direction = -1
                 this.facing = -1
-            } else if ( this.cursors.right.isDown ){
+                this.scale.x = -1
+
+                if (this.body.onFloor()) {
+                    this.animations.play('left', 10, true);
+                }
+            }
+            else if (this.cursors.right.isDown) {
                 this.body.velocity.x = this.xSpeed
-                this.direction = 1
                 this.facing = 1
-            } else {
-                this.body.velocity.x = 0
+                this.scale.x = 1
             }
         }
     }
